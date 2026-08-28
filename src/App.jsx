52fc541f3d2f04bb7097a8ccd9964e877b1bf9ef
Kennedy Mutisya <nyashtysh@gmail.com>
2026-08-28 17:09:19 +0300

    created about me conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-import Profile from './pages/Profile'
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> |{' '}
-        <Link to="/about">About</Link> |{' '}
-        <Link to="/projects">Projects</Link> |{' '}
-        <Link to="/contact">Contact</Link> |{' '}
-        <Link to="/profile">Profile</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About Us</Link> |{" "}
+        <Link to="/projects">Projects</Link> |{" "}
+        <Link to="/contact">Contact</Link> | <Link to="/profile">Profile</Link>
       </nav>
       <hr />
       <Routes>
@@ -25,5 +23,5 @@ export default function App() {
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-  )
-};
+  );
+}
